f8e514cb97b350735536785059de6cfe1e72e578
csPython <cspython@AD25TF04NGF1J-85.local>
2019-02-12 11:13:45 -0500

    add shape quad

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -2,10 +2,13 @@ from graphics import*
 
 bTriWin = GraphWin("Blue Triangle", 300, 300)
 bTriWin.setCoords(0, 0, 300, 300)
-
+#triangle
 bTri = Polygon(Point(250,300), Point(300, 300), Point(275, 280))
 bTri.setFill(color_rgb(30, 30, 230))
 bTri.draw(bTriWin)
+#quad
+bQuad = Polygon(Point(0,50), Point(50,50), Point(50,0), Point(0,0))
+bQuad.setFill(color_rgb(200, 30, 30))
+bQuad.draw(bTriWin)
 
-bTriWin.getMouse()
-bTriWin.close()
+
