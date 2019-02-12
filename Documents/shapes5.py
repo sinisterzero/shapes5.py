ae041fff522ec915853f768ee3189639252dd6a5
csPython <cspython@AD25TF04NGF1J-85.local>
2019-02-12 11:29:39 -0500

    shape don't touch the edge

--- Documents/shapes5.py
+++ Documents/shapes5.py
@@ -3,18 +3,18 @@ from graphics import*
 bTriWin = GraphWin("Blue Triangle", 300, 300)
 bTriWin.setCoords(0, 0, 300, 300)
 #triangle
-bTri = Polygon(Point(250,300), Point(300, 300), Point(275, 280))
+bTri = Polygon(Point(250,290), Point(290, 290), Point(265, 240))
 bTri.setFill(color_rgb(30, 30, 230))
 bTri.draw(bTriWin)
 #quad
-bQuad = Polygon(Point(0,50), Point(50,50), Point(50,0), Point(0,0))
+bQuad = Polygon(Point(10,60), Point(60,60), Point(60,10), Point(10,10))
 bQuad.setFill(color_rgb(200, 30, 30))
 bQuad.draw(bTriWin)
 #penta
-bPenta = Polygon(Point(0, 300), Point(50, 300), Point(50, 250), Point(25, 225), Point(0,250))
+bPenta = Polygon(Point(10, 290), Point(60, 290), Point(60, 240), Point(35, 215), Point(10,240))
 bPenta.setFill(color_rgb(200,30,200))
 bPenta.draw(bTriWin)
 #hexagon
-bHexa = Polygon(Point(200, 50), Point(225, 100),Point(275, 100), Point(300,50), Point(275,0), Point(225,0))
+bHexa = Polygon(Point(200, 60), Point(225, 110),Point(275, 110), Point(300,60), Point(275,10), Point(225,10))
 bHexa.setFill(color_rgb(30,225,40))
 bHexa.draw(bTriWin)
